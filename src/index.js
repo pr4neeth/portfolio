9d7525ab18263e5ea968d6951129431a20bfb247
Praneeth Paladugu <praneeth.paladugu2@gmail.com>
2025-12-07 22:15:49 -0600

    scroll fixes

--- src/index.js
+++ src/index.js
@@ -12,12 +12,14 @@ import Career from './components/Career';
 import CareerEnd from './components/CareerEnd';
 
 import reportWebVitals from './reportWebVitals';
+import ScrollToTop from './components/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop />
       <Navbar />
 
       <Routes>
